9d214b0c4f148406e275d66ea29c60064003c2a4
@arswo <aloewera228@mail.ru>
2021-09-11 08:47:20 +0300

    это коммит ичто тут

--- projekt_1/Program.cs
+++ projekt_1/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Threading;
 
 namespace projekt_1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Введи первое число: ");
+            float a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введи второе число: ");
+            float b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("+ - *  / % ^");
+            string c = Console.ReadLine();
+
+            if (c == "+")
+            {
+                float colv = a + b;
+                Console.WriteLine(colv);
+            }
+
+            else if (c == "-")
+            {
+                float colv = a - b;
+                Console.WriteLine(colv);
+            }
+
+            else if (c == "*")
+            {
+                float colv = a * b;
+                Console.WriteLine(colv);
+            }
+
+            else if (c == "/")
+            {
+                float colv = a / b;
+                Console.WriteLine(colv);
+            }
+
+            else if (c == "%")
+            {
+                float colv1 = a / b;
+                float q = (int)colv1;
+                float colv = colv1 - q;
+                Console.WriteLine(colv);
+            }
+
+            else if (c == "^")
+            {
+
+                float colv = 1;
+                for (int i = 0; i < b; i++)
+                colv *= a;
+                Console.WriteLine(colv);
+
+            }
+
+            else
+            {
+                Console.WriteLine("не то");
+            }
         }
     }
 }
